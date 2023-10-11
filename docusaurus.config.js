314a1d076341bf5a8f727de4dd1f8cd1d44d54f9
MizaelS <sansoresangel97@gmail.com>
2023-10-11 13:10:33 -0600

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,10 +64,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Teramont Docs',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Teramont Docs',
+          src: 'img/logo.png',
         },
         items: [
           {
