06e5fbb1f72bf39c1d74edc4716ad1bc344b6665
MizaelS <sansoresangel97@gmail.com>
2023-10-11 12:52:38 -0600

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        tcp: {
+        blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
