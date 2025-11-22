149d184d76117334f11ee04272c8cc5d138c97e5
MizaelS <sansoresangel97@gmail.com>
2025-11-22 12:26:49 -0600

    Initial

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -4,8 +4,11 @@ import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout
+      tree={source.pageTree}
+      {...baseOptions()}>
       {children}
+      
     </DocsLayout>
   );
 }
--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,9 +1,30 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
+import Logo from '@/public/favicon.ico';
+
+export const logo = (
+  <>
+    <Image
+      alt="Teramont Docs"
+      src={Logo}
+      width={32}
+      height={32}
+      sizes="32px"
+      aria-label="Teramont Docs"
+      className="shrink-0"
+    />
+  </>
+);
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
+      title: (
+        <>
+          {logo}
+          <span className="font-medium in-[.uwu]:hidden">Teramont Docs</span>
+        </>
+      ),
     },
   };
 }
