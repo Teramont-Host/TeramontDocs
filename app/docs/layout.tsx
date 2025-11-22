6724069f7be5eccf8b7d6daaf591e0b8c1408674
MizaelS <sansoresangel97@gmail.com>
2025-11-22 16:07:23 -0600

    update

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -4,11 +4,21 @@ import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout
-      tree={source.pageTree}
-      {...baseOptions()}>
-      {children}
-      
-    </DocsLayout>
+    <div 
+      className="min-h-screen"
+      style={{
+        backgroundImage: 'url(https://teramont.net/tcp/pattern.png)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundAttachment: 'fixed'
+      }}
+    >
+      <DocsLayout
+        tree={source.pageTree}
+        {...baseOptions()}>
+        {children}
+      </DocsLayout>
+    </div>
   );
 }
